be62d356e9debbd0bdc205f2ff0aeb1bad218e35
Fernando Kenzo Imami Fugihara <f205067@dac.unicamp.br>
2025-10-10 12:59:17 -0300

    remove files

--- utils/downloading.py
+++ utils/downloading.py
@@ -4,6 +4,36 @@ import requests
 import shutil
 from retry import retry
 
+def get_files_info(directory):
+    """
+    Get information about files in a specified directory.
+    This function ensures the specified directory exists, retrieves a list of all files
+    in the directory, and returns the total number of files along with their names.
+    Args:
+        directory (str): The path to the directory to inspect.
+    Returns:
+        tuple: A tuple containing:
+            - num_files (int): The total number of files in the directory.
+            - img_ids (list of str): A list of file names in the directory.
+    """
+    # Ensure the directory exists
+    os.makedirs(directory, exist_ok=True)
+
+    # Get the list of all files and directories
+    file_list = os.listdir(directory)
+
+    # Filter only files
+    file_list = [file for file in file_list if os.path.isfile(os.path.join(directory, file))]
+
+    # Get the number of files
+    num_files = len(file_list)
+
+    print(f'Total number of files: {num_files}')
+
+    img_ids = file_list  # Directly assign the list of file names
+    
+    return num_files, img_ids
+
 def getRequests(params, image, region):
     img = ee.Image(1).rename("Class").addBands(image)
     points = img.stratifiedSample(
